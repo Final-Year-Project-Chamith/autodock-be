a189ae7082b02b745dc25bd465e576be87931b98
Chamith <chamith.eos@gmail.com>
2024-11-14 19:27:17 +0530

    certbot

--- functions/func_certbot_command.go
+++ functions/func_certbot_command.go
@@ -6,14 +6,9 @@ import (
 	"os/exec"
 )
 
-
 func RunCertbotNginxCommand(domain string, email string) error {
-	
-	cmd := exec.Command("/usr/bin/certbot", "certonly", "--nginx",
-		"--non-interactive", "--agree-tos", "--email", email,
-		"-d", domain)
 
-
+	cmd := exec.Command("/usr/bin/certbot", "--nginx", "-d", domain)
 	output, err := cmd.CombinedOutput()
 	if err != nil {
 		log.Printf("Error executing certbot command: %v\nOutput: %s", err, string(output))
