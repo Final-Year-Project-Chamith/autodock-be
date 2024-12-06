f71c83a7cb95220d1979fc0ed25dd9ecba3e88b4
Chamith <chamith.eos@gmail.com>
2024-12-06 08:39:12 +0530

    addr

--- redis/connect.go
+++ redis/connect.go
@@ -3,6 +3,7 @@ package redis
 import (
 	"context"
 	"log"
+	"os"
 
 	"github.com/go-redis/redis/v8"
 )
@@ -10,17 +11,25 @@ import (
 var RedClient *redis.Client
 
 func NewRedisClient() *redis.Client {
-	client := redis.NewClient(&redis.Options{ 
-		Addr: "localhost:6379",
-		DB:   0,               
+
+	redisAddr := os.Getenv("REDIS_ADDR")
+	if redisAddr == "" {
+		redisAddr = "localhost:6379"
+		log.Println("REDIS_ADDR not set, defaulting to", redisAddr)
+	}
+
+	client := redis.NewClient(&redis.Options{
+		Addr: redisAddr, // Redis server address
+		DB:   0,         // Default database
 	})
+
 	RedClient = client
 
 	_, err := RedClient.Ping(context.Background()).Result()
 	if err != nil {
-		log.Fatalf("Failed to connect to Redis: %v", err)
+		log.Fatalf("Failed to connect to Redis at %s: %v", redisAddr, err)
 	}
 
-	log.Println("Connected to Redis successfully.")
+	log.Printf("Connected to Redis successfully at %s.\n", redisAddr)
 	return RedClient
 }
