a4504760fa6b80912b2bef45fb54923641146cc9
Chamith <chamith.eos@gmail.com>
2025-01-01 11:58:25 +0530

    logs

--- api/api_get_container_logs.go
+++ api/api_get_container_logs.go
@@ -1,15 +1,16 @@
 package api
 
 import (
+	"autodock-be/logs"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 func GetContainerLogsApi(c *fiber.Ctx) error {
 	//containerId := c.Query("containerId")
-	// _, err := logs.GetContainerLogs()
-	// if err != nil {
-	// 	return c.Status(fiber.StatusInternalServerError).JSON(map[string]string{"error": err.Error()})
-	// }
-	return c.Status(fiber.StatusOK).JSON("/logs_stor/all_containers_logs.json")
+	log, err := logs.GetContainerLogs()
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(map[string]string{"error": err.Error()})
+	}
+	return c.Status(fiber.StatusOK).JSON(log)
 }
- 
