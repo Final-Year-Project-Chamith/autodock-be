4e63875faacf44a00d2d9b5174a9ab47e7014821
Chamith <chamith.eos@gmail.com>
2025-02-24 12:47:13 +0530

    execute certbot command when nginx is temperaoloy stopped

--- functions/func_certbot_command.go
+++ functions/func_certbot_command.go
@@ -6,20 +6,15 @@ import (
 )
 
 func GenerateSSL(domain string, email string) error {
-	fmt.Println("stop niginx 1")
-	if err := StopNginxConfig(); err != nil{
-		return err
-	}
+	fmt.Println("starting.............")
 	fmt.Println("stop niginx 2")
-	cmd := exec.Command("certbot", "certonly", "--standalone", "-d", domain, "--non-interactive", "--agree-tos", "--chamith.eos@gmail.com", email)
-
+	cmd := exec.Command("certbot", "--nginx", "-d", domain, "--non-interactive", "--agree-tos", "--email", "chamith.eos@gmail.com")
 	output, err := cmd.CombinedOutput()
 	if err != nil {
 		return fmt.Errorf("failed to generate SSL certificate: %v\nOutput: %s", err, string(output))
 	}
-	if err := StartNginxConfig(); err != nil{
-		return err
-	}
+	fmt.Println("done.............")
+	
 	fmt.Println("SSL certificate generated successfully!")
 	fmt.Println(string(output))
 	return nil
