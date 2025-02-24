9ac4a96184bb305c8a2da959fd78801b7d610f21
Chamith <chamith.eos@gmail.com>
2025-02-24 08:44:17 +0530

    execute certbot command when nginx is temperaoloy stopped

--- functions/func_certbot_command.go
+++ functions/func_certbot_command.go
@@ -2,34 +2,23 @@ package functions
 
 import (
 	"fmt"
-	"os"
 	"os/exec"
 )
 
-// GenerateCert writes a certbot script inside the mounted folder and executes it on the VM
-func GenerateCert(domain string) error {
-
-	// Define paths
-	containerScriptPath := "/certbot/certbot_" + domain + ".sh"     // Inside container (mounted)
-	vmScriptPath := "/home/admin/certbot/certbot_" + domain + ".sh" // Inside VM
-
-	if err := GenerateCertbotSHFile(domain); err != nil {
+func generateSSL(domain string, email string) error {
+	if err := StopNginxConfig(); err != nil{
 		return err
 	}
+	cmd := exec.Command("certbot", "certonly", "--standalone", "-d", domain, "--non-interactive", "--agree-tos", "--chamith.eos@gmail.com", email)
 
-	// Step 2: Execute the script on the VM
-	cmd := exec.Command("bash", "-c", vmScriptPath)
 	output, err := cmd.CombinedOutput()
 	if err != nil {
-		return fmt.Errorf("error executing script on VM: %v\nOutput: %s", err, string(output))
+		return fmt.Errorf("failed to generate SSL certificate: %v\nOutput: %s", err, string(output))
 	}
-
-	// Step 3: Delete the script after execution from VM
-	if err := os.Remove(containerScriptPath); err != nil {
-		return fmt.Errorf("failed to delete script file on VM: %v", err)
+	if err := StartNginxConfig(); err != nil{
+		return err
 	}
-
-	fmt.Println("✅ SSL Certificate generated successfully for:", domain)
-	fmt.Println("📜 Output:", string(output))
+	fmt.Println("SSL certificate generated successfully!")
+	fmt.Println(string(output))
 	return nil
 }
--- functions/func_test_nginx_conf.go
+++ functions/func_test_nginx_conf.go
@@ -16,3 +16,27 @@ func TestNginxConfig() error {
 	fmt.Printf("Nginx test succeeded: %s\n", string(output))
 	return nil
 }
+
+func StopNginxConfig() error {
+	stopCmd := exec.Command("systemctl", "stop", "nginx")
+	stopOutput, stopErr := stopCmd.CombinedOutput()
+	if stopErr != nil {
+		fmt.Printf("Failed to stop Nginx: %s\n", string(stopOutput))
+		return stopErr
+	}
+
+	fmt.Println("Nginx stopped successfully.")
+	return nil
+}
+func StartNginxConfig() error {
+	stopCmd := exec.Command("systemctl", "start", "nginx")
+	stopOutput, stopErr := stopCmd.CombinedOutput()
+	if stopErr != nil {
+		fmt.Printf("Failed to stop Nginx: %s\n", string(stopOutput))
+		return stopErr
+	}
+
+	fmt.Println("Nginx stopped successfully.")
+	return nil
+}
+
