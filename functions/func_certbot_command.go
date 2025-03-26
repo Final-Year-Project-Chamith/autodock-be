791ab62762d88a368c01b4007f86567b6f625eb4
Chamith <chamith.eos@gmail.com>
2025-03-26 11:21:42 +0530

    certbot command

--- functions/func_certbot_command.go
+++ functions/func_certbot_command.go
@@ -14,8 +14,21 @@ func GenerateSSL(domain string, email string) error {
 		return fmt.Errorf("failed to generate SSL certificate: %v\nOutput: %s", err, string(output))
 	}
 	fmt.Println("done.............")
-	
+
 	fmt.Println("SSL certificate generated successfully!")
 	fmt.Println(string(output))
 	return nil
+}
+func GenerateSSLCert(domain string) (string, error) {
+	if domain == "" {
+		return "", fmt.Errorf("domain is required")
+	}
+
+	cmd := exec.Command("certbot", "--nginx", "-d", domain)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return "", fmt.Errorf("error running certbot: %v\nOutput: %s", err, output)
+	}
+
+	return string(output), nil
 }
